7c4f6874517627727775aa83758f6cd4455c1003
Ruslan <rnzamuraev@gmail.com>
2023-02-22 20:28:05 +0300

    added file classes and actions

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,22 @@ import $ from "../lib/lib";
 
 console.log("hello!");
 
-const h1 = $("h1").hide().show("flex");
-// const h1 = $("h1").show();
+const h1 = $("h1").hide().show("flex").addClass("active");
+if (h1.containsClass("active")) {
+  $(".btn").on("click", function () {
+    console.log(this);
+    this.classList.add("sddvsd");
+    console.log($(".icon"));
+    $(".icon").toggleClass("active");
+    // $(this).toggleClass("active");
+  });
+  console.log(h1.containsClass("active"));
+}
+// $(".btn").off("click", btnClick);
 console.log(h1);
+
+function btnClick() {
+  console.log(this);
+  console.log(this.tagName);
+  this.toggleClass("active");
+}
--- src/lib/core.js
+++ src/lib/core.js
@@ -1,26 +1,28 @@
 const $ = function (selector) {
-  // console.log(new $.prototype());
-  console.log(new $.prototype.init());
   return new $.prototype.init(selector);
 };
 
 $.prototype.init = function (selector) {
+  console.log(selector);
   if (!selector) {
-    console.log(this);
     return this; // {}
   }
 
-  // Object.assign(this, document.querySelectorAll(selector));
+  if (selector.tagName) {
+    console.log(" Object.assign(this, selector);");
+    // Object.assign(this, selector);
+    this[0] = selector;
+    this.length = 1;
+    return this;
+  }
+
   Object.assign(this, document.querySelectorAll(selector));
   this.length = document.querySelectorAll(selector).length;
 
   return this;
 };
-console.log($.prototype);
+
 $.prototype.init.prototype = $.prototype;
-console.log($.prototype);
-console.log($.prototype.init);
-console.log($.prototype.init.prototype);
 
 window.$ = $;
 
