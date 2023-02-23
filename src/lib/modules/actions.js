2219c1cea0a85d12be237f02a9384d1dda724d27
Ruslan <rnzamuraev@gmail.com>
2023-02-23 11:43:44 +0300

    first commit

--- src/lib/modules/actions.js
+++ src/lib/modules/actions.js
@@ -1,1 +1,11 @@
 import $ from "../core";
+
+$.prototype.html = function (content) {
+  for (let i = 0; i < this.length; i++) {
+    if (!content) {
+      return this[i].innerHTML;
+    }
+    this[i].innerHTML = content;
+  }
+  return this;
+};
